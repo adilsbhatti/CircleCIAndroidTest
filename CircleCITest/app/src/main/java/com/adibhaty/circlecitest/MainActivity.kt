da58c6ff98b666bd69805d05087f1ac867a4f91c
adilsbhatti <asbhatty150@gmail.com>
2018-11-12 21:25:52 +1100

    added button

--- CircleCITest/app/src/main/java/com/adibhaty/circlecitest/MainActivity.kt
+++ CircleCITest/app/src/main/java/com/adibhaty/circlecitest/MainActivity.kt
@@ -1,12 +1,28 @@
 package com.adibhaty.circlecitest
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var bToast : Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        bToast = findViewById(R.id.bToast)
+
+
+        bToast.setOnClickListener(View.OnClickListener() {
+
+            Toast.makeText(applicationContext, "this si toast", 1).show()
+        })
+
+
     }
 }
